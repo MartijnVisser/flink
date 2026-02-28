ebce69867d74bf1a09d7efb24849f6f74b592e28
Martijn Visser <2989614+MartijnVisser@users.noreply.github.com>
2026-02-28 09:41:31 -0800

    [FLINK-38223][tests] Add MainThreadExecutionGraphTestUtils wrapper
    Thin wrapper around TestingComponentMainThreadExecutor that provides
    thread-safe versions of ExecutionGraphTestUtils state mutation methods.
    Accepts the executor once at construction so each call site is concise.

---
+++ flink-runtime/src/test/java/org/apache/flink/runtime/executiongraph/MainThreadExecutionGraphTestUtils.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.runtime.executiongraph;
+
+import org.apache.flink.runtime.concurrent.ComponentMainThreadExecutor;
+import org.apache.flink.runtime.jobgraph.JobVertexID;
+import org.apache.flink.util.function.SupplierWithException;
+import org.apache.flink.util.function.ThrowingRunnable;
+
+/**
+ * Thread-safe wrapper around {@link ExecutionGraphTestUtils} that dispatches all state mutation
+ * calls through a {@link TestingComponentMainThreadExecutor}.
+ *
+ * <p>Use this in tests that create a proper main thread executor (via {@link
+ * TestingComponentMainThreadExecutor.Extension}) instead of calling {@link
+ * ExecutionGraphTestUtils} directly from the test thread.
+ */
+public class MainThreadExecutionGraphTestUtils {
+
+    private final TestingComponentMainThreadExecutor mainThreadExecutor;
+
+    public MainThreadExecutionGraphTestUtils(
+            TestingComponentMainThreadExecutor mainThreadExecutor) {
+        this.mainThreadExecutor = mainThreadExecutor;
+    }
+
+    public ComponentMainThreadExecutor getMainThreadExecutor() {
+        return mainThreadExecutor.getMainThreadExecutor();
+    }
+
+    public void switchAllVerticesToRunning(ExecutionGraph eg) {
+        mainThreadExecutor.execute(
+                () -> ExecutionGraphTestUtils.switchAllVerticesToRunning(eg));
+    }
+
+    public void completeCancellingForAllVertices(ExecutionGraph eg) {
+        mainThreadExecutor.execute(
+                () -> ExecutionGraphTestUtils.completeCancellingForAllVertices(eg));
+    }
+
+    public void finishAllVertices(ExecutionGraph eg) {
+        mainThreadExecutor.execute(() -> ExecutionGraphTestUtils.finishAllVertices(eg));
+    }
+
+    public void finishJobVertex(ExecutionGraph eg, JobVertexID jobVertexId) {
+        mainThreadExecutor.execute(
+                () -> ExecutionGraphTestUtils.finishJobVertex(eg, jobVertexId));
+    }
+
+    public void switchToRunning(ExecutionGraph eg) {
+        mainThreadExecutor.execute(() -> ExecutionGraphTestUtils.switchToRunning(eg));
+    }
+
+    /** Dispatch any operation that needs the main thread. */
+    public void execute(ThrowingRunnable<Throwable> operation) {
+        mainThreadExecutor.execute(operation);
+    }
+
+    /** Dispatch any operation that needs the main thread and returns a result. */
+    public <T> T execute(SupplierWithException<T, Throwable> operation) {
+        return mainThreadExecutor.execute(operation);
+    }
+}
